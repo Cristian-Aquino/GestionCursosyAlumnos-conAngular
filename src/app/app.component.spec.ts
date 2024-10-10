a966745671cfbb6a6b36ccff17c2f34f27e8ca57
cristian-aquino <cristian.aquino0412@gmail.com>
2024-10-09 22:39:36 -0300

    Primera Entrega PF-Aquino

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '1PF-Aquino'`, () => {
+  it(`should have as title 'PF-Aquino'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('1PF-Aquino');
+    expect(app.title).toEqual('PF-Aquino');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 1PF-Aquino');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PF-Aquino');
   });
 });
